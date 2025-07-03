9e1aab15925e7c7925dbdc30c4cf619537225394
AggiusVS <aggiusvellastefano@gmail.com>
2025-07-03 19:25:51 +0200

    trova_massimo

---
+++ src/trova_massimo.java
@@ -0,0 +1,14 @@
+public class trova_massimo {
+    public static void main(String[] args) {
+        int[] numeri = {1, 2, 3, 4, 5, 257, 7, 8, 9, 170};
+        int massimo = 0;
+
+        for (int i = 0; i < numeri.length; i++) {
+            if (numeri[i] > massimo) {
+                massimo = numeri[i];
+            }
+            
+        }
+        System.out.println("Il numero massimo dell'array è: " + massimo);
+    }
+}
